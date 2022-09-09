3c91a970beeac0a7f733af4b6614828d759d5ae7
karyawati <95742782+karyawati@users.noreply.github.com>
2022-09-09 23:32:32 +0700

    Logo Utama

---
+++ index.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="id">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  
+ <style>
+h3 {
+  color: white;
+  text-shadow: 1px 1px 2px black, 0 0 25px red, 0 0 5px red;
+}
+
+h5 {
+  color: red;
+  text-shadow: 1px 1px 2px black, 0 0 25px white, 0 0 5px white;
+}
+
+a, p {
+    color: #a70000;
+}
+
+a:hover {
+    color: red;
+}
+
+</style>
+  
+    <title>NGOPI.id - Ngonline Paket Instan</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-dark navbar-expand-lg dark  sticky-top" style="background-color: #a70000;">
+ <div class="container-fluid">
+<a class="navbar-brand" href="#"> <img src="ngopi-id.png" width="25%" height="25%" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="https://ngopi.id"><img src="id.png"width="35" height="35" alt=".id"></a>
+        <a class="nav-link" href="https://ngopi.net"><h2>.net</h2></a>
+        <a class="nav-link" href="https://ngopi.org"><h2>.org</h2></a>
+        <a class="nav-link" href="https://ngopi.biz"><h2>.biz</h2></a>
+</div>
+    </div>
+  </div>
+</nav>
+  
+<div class="text-center"><font color="#a70000">
+  ••• <strong>Sebelum melakukan pemesanan, pastikan Anda telah membaca <a href="aturan.php">PERATURAN</a> layanan kami </strong>  ••• </font>
+</div><section id="services" style="background-color: #a70000">
+    <div class="container">
+    <div class="row">
+    <div class="col">
+        <p class="font-weight-bold text-white"><h1 class="text-white text-center">LAYANAN KAMI</h1></p>
+        <h3>Hos Web</h3></p>
+        <p class="text-white ">NGOPI.id menyediakan layanan hos web sesuai dengan kebutuhan Anda. Informasikan kebutuhan Anda kepada kami, kami akan mengajukan penawaran yang menarik kepada Anda dalam waktu maksimal 48 jam. Alternatifnya, Anda dapat menggunakan paket yang sudah jadi</p>
+        <p class="text-center text-white"><strong>Mulai Rp500/hari | 500MB | DirectAdmin</strong></p>
+        
+        
+        <h3>Nama Domain</h3></p>
+        <p class="text-white">NGOPI.id menyediakan jasa registrasi nama domain Internasional dan Indonesia.</p>
+        <p class="text-white">Ekstensi yang dapat kami bantu daftarkan:</p>
+        <p class="text-white"><em>.com • .net • .org • .biz • .info • .asia • .id • .co.id • .or.id • .ac.id • .web.id • .sch.id • .biz.id • .my.id • .ponpes.id </em></p>
+
+        
+        <p class="text-center text-white"><strong>Mulai Rp20.000/tahun | .my.id | Kini Termasuk Hos Web 500MB </strong></p>
+        
+
+        <h3>Zoom Meeting Pro 300 Partisipan</h3></p>
+        <p class="text-white">NGOPI.id menawarkan jasa akun pertemuan daring Zoom 300 peserta dengan akun @zoom.ngopi.net.</p>
+        <p class="text-white">Kami menggunakan layanan Telkomsel untuk menunjang layanan kami</em></p>
+
+        
+        <p class="text-center text-white"><strong>Rp120.000/bulan </strong></p>
+        
+                </div>
+
+
+    <div class="col">
+    <p></p>
+    
+    <div class="card">
+  <div class="card-body">
+  <h5 class="card-title">B-li. @ccess</h5>
+ 
+    <p class="card-text">B-li. @ccess (Akses B-li) adalah portal layanan pelanggan yang terpisah dari laman utama ini untuk mengelola layanan. Pesan dan kelola layanan dari NGOPI.id di sini.</p>
+  <a href="https://b-li.com" class="btn btn-outline-danger">Masuk ke portal B-li. @ccess sekarang >> </a>
+  </div>
+</div>
+<p></p>
+    
+<div class="card">
+    <div class="card-body">
+    <h5 class="card-title">Kontak</h5>
+    <p class="card-text">WhatsApp: 088-8383-1496 <br>
+    Telegram: @fonsel <br>
+    Zoom PMI: 98-8383-1496 <br>
+    <em>Silakan lakukan mengatur jadwal via WhatsApp/Telegram terlebih dahulu</em><br>
+    Surel: info@ngopi.id <br>
+    Alamat: Tenaga 2B Kav. 4 Malang <br>
+    </p>
+  </div>
+</div>
+
+    
+    </div>
+    </div>
+    
+
+</section>
+<p></p>
+<section id="footer" style="background-color: #a70000">
+    <div class="container">
+    <div class="row text-center">
+<p class="footer text-white text-center mt-2"><strong>Untuk meningkatkan pengalaman pengguna layanan, kami menggunakan <em>cookie</em> pada sebagian laman kami. Bila Anda tetap melanjutkan penjelajahan, kami anggap Anda menyetujui penggunaan <em>cookie</em></strong>  </p>
+</div>
+</div>
+</section>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+
+  </body>
+</html>
