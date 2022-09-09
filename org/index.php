401b353af30a34bb57fb92091cc466efd82d8615
karyawati <95742782+karyawati@users.noreply.github.com>
2022-09-09 23:48:56 +0700

    NGOPI.ORG

---
+++ org/index.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="id">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  
+ <style>
+h3 {
+  color: white;
+  text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px blue;
+}
+
+h5 {
+  color: blue;
+  text-shadow: 1px 1px 2px black, 0 0 25px white, 0 0 5px white;
+}
+
+a, p {
+    color: #0000a7;
+}
+
+a:hover {
+    color: blue;
+}
+
+</style>
+  
+    <title>NGOPI.org - PANDUAN NGOPI</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-dark navbar-expand-lg dark  sticky-top" style="background-color: #0000a7;">
+ <div class="container-fluid">
+<a class="navbar-brand" href="#"> <img src="https://ngopi.id/ngopi-id.png" width="25%" height="25%" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="https://ngopi.id"><h2>.id</h2></a>
+        <a class="nav-link" href="https://ngopi.net"><h2>.net</h2></a>
+        <a class="nav-link active" aria-current="page" href="https://ngopi.org"><img src="org.png"width="35" height="35" alt=".org"></a>
+        <a class="nav-link" href="https://ngopi.biz"><h2>.biz</h2></a>
+</div>
+    </div>
+  </div>
+</nav>
+  
+<div class="text-center"><font color="#0000a7">
+  ••• <strong>SEGERA HADIR! Hos Web Gratis 28 hari (1 GB, CPanel, Server Singapura) untuk keperluan showcase tugas akhir ••• </font>
+</div><section id="services" style="background-color: #0000a7">
+    <div class="container">
+    <div class="row">
+    <div class="col">
+        <p class="font-weight-bold text-white"><h1 class="text-white text-center">PANDUAN SINGKAT</h1></p>
+        <h3>Nama Server Hos Web</h3></p>
+        <p class="text-white ">Bila Anda mendaftarkan domain di NGOPI.id, nameserver telah diset otomatis. Bila nama domain tidak diregistrasi di kami, ubahlah dengan nameserver berikut:</p>
+        <p class="text-center text-white"><strong>luwak.ngopi.net</strong></p>
+        <p class="text-center text-white"><strong>tuwus.ngopi.net</strong></p>
+        
+        <h3>Email Login Zoom</h3></p>
+        <p class="text-white">Untuk mempermudah pengecekan bila terjadi kendala, layanan zoom pro dari ngopi.id, selalu menggunakan akhiran berikut:</p>
+        <p class="text-center text-white"><strong>@zoom.ngopi.net (terdaftar sesudah 9 September 2022)</strong></p> 
+        <p class="text-center text-white">@ngopi.net (terdaftar sampai dengan 9 September 2022)</p>
+        
+                </div>
+
+
+    <div class="col">
+    <p></p>
+    
+  <div class="card">
+  <div class="card-body">
+  <h5 class="card-title">DirectAdmin</h5>
+  <p class="card-text">DirectAdmin adalah panel untuk mengelola berkas di hos web yang kami pakai di server Indonesia.</p>
+  <a href="https://directadmin.com" class="btn btn-outline-primary">Baca panduan DirectAdmin sekarang >></a>
+  </div>
+</div>
+<p></p>
+    
+<div class="card">
+  <div class="card-body">
+  <h5 class="card-title">cPanel</h5>
+  <p class="card-text">cPanel adalah panel untuk mengelola berkas di hos web yang kami pakai di server Indonesia.</p>
+  <a href="https://cpanel.net" class="btn btn-outline-primary">Baca panduan cPanel sekarang >></a>
+  </div>
+</div>
+<p></p>
+
+<div class="card">
+  <div class="card-body">
+  <h5 class="card-title">Zoom</h5>
+  <p class="card-text">Zoom adalah layanan pertemuan online terpopuler.</p>
+  <a href="https://telkomsel.com/zoom" class="btn btn-outline-primary">Baca panduan Zoom Pro sekarang >></a>
+  </div>
+</div>
+<p></p>
+    
+    </div>
+    </div>
+    
+
+</section>
+<p></p>
+<section id="footer" style="background-color: #0000a7">
+    <div class="container">
+    <div class="row text-center">
+<p class="footer text-white text-center mt-2"><strong>Untuk meningkatkan pengalaman pengguna layanan, kami menggunakan <em>cookie</em> pada sebagian laman kami. Bila Anda tetap melanjutkan penjelajahan, kami anggap Anda menyetujui penggunaan <em>cookie</em></strong>  </p>
+</div>
+</div>
+</section>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+
+  </body>
+</html>
