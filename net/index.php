4a901e27b727e0023cf893cd394bb41339d5c5d3
karyawati <95742782+karyawati@users.noreply.github.com>
2022-09-09 23:42:01 +0700

    NGOPI.NET

--- net/index.php
+++ net/index.php
@@ -1,1 +1,81 @@
+<!doctype html>
+<html lang="id">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  
+ <style>
+h3 {
+  color: white;
+  text-shadow: 1px 1px 2px black, 0 0 25px yellow, 0 0 5px yellow;
+}
+
+h5 {
+  color: yellow;
+  text-shadow: 1px 1px 2px black, 0 0 25px white, 0 0 5px white;
+}
+
+a, p {
+    color: #FFC300;
+}
+
+a:hover {
+    color: yellow;
+}
+
+</style>
+  
+    <title>NGOPI.net - Internet Gratis di Malang</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-dark navbar-expand-lg dark  sticky-top" style="background-color: #FFC300;">
+ <div class="container-fluid">
+<a class="navbar-brand" href="#"> <img src="https://ngopi.id/ngopi-id.png" width="25%" height="25%" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="https://ngopi.id"><h2>.id</h2></a>
+        <a class="nav-link active" aria-current="page" href="https://ngopi.net"><img src="net.png"width="35" height="35" alt=".net">
+        <a class="nav-link" href="https://ngopi.org"><h2>.org</h2></a>
+        <a class="nav-link" href="https://ngopi.biz"><h2>.biz</h2></a>
+</div>
+    </div>
+  </div>
+</nav>
+  
+<div class="text-center"><font color="#000000">
+  ••• <strong>UNTUK SEMENTARA, KAMI HANYA MENYEDIAKAN KUOTA BULANAN 200GB (SHARED) ••• </font>
+</div><section id="services" style="background-color: #FFC300">
+    <div class="container">
+    <div class="row">
+
+        <p class="font-weight-bold"><h1 class="text-center">Informasi WI-FI GRATIS</h1></p>
+        <p class="text-center text-black">Temukan SID ini di sekitar markas kami:</p>
+        <center><h3>NGOPI.NET</h3></center>
+        </p>
+        
+        
+                </div>
+
+
+</section>
+<p></p>
+<section id="footer" style="background-color: #FFC300">
+    <div class="container">
+    <div class="row text-center">
+<p class="footer text-black text-center mt-2"><strong>Untuk meningkatkan pengalaman pengguna layanan, kami menggunakan <em>cookie</em> pada sebagian laman kami. Bila Anda tetap melanjutkan penjelajahan, kami anggap Anda menyetujui penggunaan <em>cookie</em></strong>  </p>
+</div>
+</div>
+</section>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+
+  </body>
+</html>
